04fae8ca321db688e22184bba08e39a68a54c62c
Aviv <avivhauyn@gmail.com>
2024-05-09 18:37:01 +0300

    Base projects
    Made basic Folders
    Added basic utility scripts
    Added basic editor tools

---
+++ Assets/_Scripts/Utility/VectorUtility.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class VectorUtility : MonoBehaviour
+{
+    static public Vector3 FlattenVector(Vector3 vectorToFlatten)
+    {
+        return new Vector3(vectorToFlatten.x, 0f, vectorToFlatten.z);
+    }
+    static public Vector3 FlattenVector(Vector3 vectorToFlatten, float desiredYValue)
+    {
+        return new Vector3(vectorToFlatten.x, desiredYValue, vectorToFlatten.z);
+    }
+    static public Vector3 GetDirection(Vector3 from, Vector3 to)
+    {
+        return (to - from).normalized;
+    }
+    static public Vector3 Round(Vector3 vector3, int decimalPlaces)
+    {
+        float multiplier = 1;
+        for (int i = 0; i < decimalPlaces; i++)
+        {
+            multiplier *= 10f;
+        }
+        return new Vector3(
+            Mathf.Round(vector3.x * multiplier) / multiplier,
+            Mathf.Round(vector3.y * multiplier) / multiplier,
+            Mathf.Round(vector3.z * multiplier) / multiplier);
+    }
+    static public Vector3 Round(Vector3 vector3)
+    {
+        return new Vector3(
+            Mathf.Round(vector3.x),
+            Mathf.Round(vector3.y),
+            Mathf.Round(vector3.z));
+    }
+    static public Vector3 FromColor(Color color)
+    {
+        return new Vector3(color.r, color.g, color.b);
+    }
+    static public Color ToColor(Vector3 color)
+    {
+        return new Color(color.x, color.y, color.z);
+    }
+
+}
+
