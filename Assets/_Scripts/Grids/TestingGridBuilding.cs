8f3b217211531518017897b392d2946508943824
Aviv <avivhauyn@gmail.com>
2024-05-14 11:53:19 +0300

    Adds script to test grid system

---
+++ Assets/_Scripts/Grids/TestingGridBuilding.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGridBuilding : MonoBehaviour
+{
+    public List<Cell> test;
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Cell cell = hit.transform.GetComponentInParent<Cell>();
+            if(cell == null) Debug.Log("Cell null");
+            if(cell.grid == null) cell.ReconnectWithGrid();
+            if(cell.grid.GetGrids(new Vector2Int(cell.x, cell.y), 2, 2, out test) == false)  Debug.Log("NO space");
+            foreach (Cell c in test)
+            {
+                c.Placed = true;
+            }
+        }
+    }
+}
