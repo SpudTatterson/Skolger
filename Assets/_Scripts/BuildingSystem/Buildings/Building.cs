461ae88de9dce3a7e8facb786e7b27e1311a51f4
Aviv <avivhauyn@gmail.com>
2024-05-15 23:41:07 +0300

    Upgraded Building scriptable object

--- Assets/_Scripts/BuildingSystem/Buildings/Building.cs
+++ Assets/_Scripts/BuildingSystem/Buildings/Building.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Building")]
 public class Building : ScriptableObject
 {
-  public int length = 1;
-  public int width = 1;
-  public List<MaterialCost> costs = new List<MaterialCost>();   
+  public int xSize = 1;
+  public int ySize = 1;
+  public List<MaterialCost> costs = new List<MaterialCost>();
+  public GameObject visualGO;   
+  public bool walkable = false;
+  public bool takesFullCell = true;
 }
