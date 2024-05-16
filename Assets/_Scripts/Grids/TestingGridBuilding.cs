1d82c66192a7bf9f2093d8c6d645e582b7ceff92
Aviv <avivhauyn@gmail.com>
2024-05-16 15:14:05 +0300

    deleted old debugs

--- Assets/_Scripts/Grids/TestingGridBuilding.cs
+++ Assets/_Scripts/Grids/TestingGridBuilding.cs
@@ -12,13 +12,8 @@ public class TestingGridBuilding : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit) && Input.GetKeyDown(KeyCode.Mouse0))
         {
-
-            Debug.Log(hit.transform.name);
             GridManager grid = hit.transform.GetComponentInParent<GridManager>();
-            if(grid == null) Debug.Log("No grid found");
             Cell cell = grid.GetCellFromPosition(hit.point);
-            if(cell == null) Debug.Log("No cell found");
-            Debug.Log(cell.position);
             bool cellsExist = grid.TryGetCells(new Vector2Int(cell.x, cell.y), building.xSize, building.ySize, out cells);
             bool cellsFree = true;
             foreach(Cell c in cells)
@@ -30,7 +25,7 @@ public class TestingGridBuilding : MonoBehaviour
             {
                 InventoryManager.instance.UseMaterials(building.costs);
                 Debug.Log(InventoryManager.instance.CheckAmount(MaterialType.Wood));
-                Instantiate(building.visualGO, cell.position, Quaternion.identity);
+                Instantiate(building.building, cell.position, Quaternion.identity);
                 foreach (Cell c in cells)
                 {
                     c.inUse = building.takesFullCell;
