03c50845bf747a48f4fe93df53be16cc0d9e7ad6
Aviv <avivhauyn@gmail.com>
2024-05-16 15:02:40 +0300

    Separated logic From building class
    Building class now inherits form base placeable class that will hold all generic info about anything the player will place in the world

--- Assets/_Scripts/BuildingSystem/Buildings/Building.cs
+++ Assets/_Scripts/BuildingSystem/Buildings/Building.cs
@@ -1,13 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Building")]
-public class Building : ScriptableObject
+[CreateAssetMenu(menuName = "Building/Building")]
+public class Building : Placeable
 {
-  public int xSize = 1;
-  public int ySize = 1;
   public List<MaterialCost> costs = new List<MaterialCost>();
   public GameObject visualGO;   
   public bool walkable = false;
-  public bool takesFullCell = true;
 }
---
+++ Assets/_Scripts/BuildingSystem/Buildings/Placeable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : ScriptableObject
+{
+    public int xSize = 1;
+    public int ySize = 1;
+}
