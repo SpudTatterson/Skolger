837680ca1d38f0669ce57823b58b42f1ef933ab4
Aviv <avivhauyn@gmail.com>
2024-05-19 12:54:57 +0300

    Started work on Gather system
    Setup base classes and interfaces

---
+++ Assets/_Scripts/GatherSystem/IHarvestable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHarvestable 
+{
+    public IEnumerator StartHarvesting();
+
+    public bool IsBeingHarvested();
+}
