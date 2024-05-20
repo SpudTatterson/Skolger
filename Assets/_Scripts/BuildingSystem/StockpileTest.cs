6f63ea2629a660d83e0866e6fe38ad523aa7218a
Aviv <avivhauyn@gmail.com>
2024-05-20 12:28:49 +0300

    Updated stockpile test to new system

--- Assets/_Scripts/BuildingSystem/StockpileTest.cs
+++ Assets/_Scripts/BuildingSystem/StockpileTest.cs
@@ -7,13 +7,39 @@ public class StockpileTest : MonoBehaviour
     public List<Cell> cells;
     [SerializeField] Vector2Int size = new Vector2Int(1, 1);
 
-    bool stockpileMade = false;
+    bool makingStockpile = false;
 
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit itemHit;
+        if (Physics.Raycast(ray, out itemHit, 100, LayerManager.instance.ItemLayerMask) && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ItemObject item = itemHit.transform.GetComponent<ItemObject>();
+            Stockpile stockpile = InventoryManager.instance.stockpiles[0];
+            Cell cell;
+            if (stockpile.GetEmptyCell(out cell)) // small bug - cannot merge items if no empty cells are found
+            {
+                if (item.CheckIfInStockpile(out _))
+                {
+                    return;
+                }
+                else
+                {
+                    stockpile.AddItem(item);
+                    Destroy(itemHit.transform.gameObject);
+                }
+            }
+            else
+            {
+                Debug.Log("No empty spots found in stockpile");
+            }
+
+        }
+
+        if(!makingStockpile) return;
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 500f, LayerManager.instance.GroundLayerMask) && Input.GetKeyDown(KeyCode.Mouse0) && stockpileMade == false)
+        if (Physics.Raycast(ray, out hit, 500f, LayerManager.instance.GroundLayerMask) && Input.GetKeyDown(KeyCode.Mouse0))
         {
             GridManager grid = hit.transform.GetComponentInParent<GridManager>();
             Cell cell = grid.GetCellFromPosition(hit.point);
@@ -26,6 +52,7 @@ public class StockpileTest : MonoBehaviour
             }
             if (cellsExist && cellsFree)
             {
+                makingStockpile = false;
                 GameObject stockpileGO = new GameObject("Stockpile");
                 Stockpile stockpile = stockpileGO.AddComponent<Stockpile>();
                 stockpile.Initialize(size.x, size.y, cells, cell.position, 1);
@@ -34,35 +61,12 @@ public class StockpileTest : MonoBehaviour
                     c.inUse = true;
                     c.Walkable = true;
                 }
-                stockpileMade = true;
             }
         }
-        RaycastHit itemHit;
-        if (Physics.Raycast(ray, out itemHit, 100, LayerManager.instance.ItemLayerMask) && Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            ItemObject item = itemHit.transform.GetComponent<ItemObject>();
-            Stockpile stockpile = InventoryManager.instance.stockpiles[0];
-            Cell cell;
-            if (stockpile.GetEmptyCell(out cell))
-            {
-                Debug.Log("yes");
-                stockpile.AddItem(cell, item);
-                // Destroy(item.visualGO); // if we destroy it we lose data we need to either move it or figure out something else
-                Stockpile itemStockpile;
-                if (item.CheckIfInStockpile(out itemStockpile))
-                {
-                    itemStockpile.TakeItem(cell);
-                }
-                else
-                { 
-                    Destroy(itemHit.transform.gameObject);
-                }
-            }
-            else
-            {
-                Debug.Log("No empty spots found in stockpile");
-            }
+    }
 
-        }
+    public void StartMakingStockPile()
+    {
+        makingStockpile = true;
     }
 }
