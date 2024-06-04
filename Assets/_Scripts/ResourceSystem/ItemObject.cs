cef629ce640dd0d798ddcfb8731db46f9d52094f
Aviv <avivhauyn@gmail.com>
2024-06-04 13:19:26 +0300

    Fixed Allow Logic on Items

--- Assets/_Scripts/ResourceSystem/ItemObject.cs
+++ Assets/_Scripts/ResourceSystem/ItemObject.cs
@@ -10,7 +10,7 @@ public class ItemObject : MonoBehaviour, ISelectable, IAllowable
     [SerializeField] bool doManualInitialized = false;
     [SerializeField] bool inStockpile = false;
     [SerializeField] SelectionType selectionType;
-    [SerializeField] bool allowed = true;
+    [SerializeField] bool allowedOnInit = true;
 
     public int amount { get; private set; }
 
@@ -18,14 +18,15 @@ public class ItemObject : MonoBehaviour, ISelectable, IAllowable
     Stockpile currentStockpile;
     public GameObject visualGO { get; private set; }
     public Cell occupiedCell { get; private set; }
+    public bool allowed { get; private set; }
 
-    
+
 
 
     void Start()
     {
         if (doManualInitialized)
-            Initialize(itemData, amount);
+            Initialize(itemData, amount, allowedOnInit);
     }
 
     public void Initialize(ItemData itemData, int amount, bool allowed = true, GameObject visualGO = null, bool inStockpile = false, Stockpile stockpile = null)
@@ -34,8 +35,7 @@ public class ItemObject : MonoBehaviour, ISelectable, IAllowable
 
         this.amount = amount;
         stackSize = itemData.stackSize;
-        this.allowed = allowed;
-        if(allowed) OnAllow();
+        if (allowed) OnAllow();
         else OnDisallow();
         this.visualGO = visualGO;
         this.inStockpile = inStockpile;
@@ -95,7 +95,7 @@ public class ItemObject : MonoBehaviour, ISelectable, IAllowable
 
         return takeAmount;
     }
-    public static ItemObject MakeInstance(ItemData itemData, int amount, Vector3 position,bool allowed = true, Transform parent = null, bool inStockpile = false, Stockpile stockpile = null)
+    public static ItemObject MakeInstance(ItemData itemData, int amount, Vector3 position, bool allowed = true, Transform parent = null, bool inStockpile = false, Stockpile stockpile = null)
     {
         GameObject visualGO = Instantiate(itemData.visual, position, Quaternion.identity);
 
@@ -138,9 +138,9 @@ public class ItemObject : MonoBehaviour, ISelectable, IAllowable
         amount = this.amount;
         return itemData.itemName;
     }
-    
+
     #endregion
-    
+
     #region IAllowable 
 
     public void OnAllow()
