0c786634d83d7ac5f77feb75e89fb2b61612c497
Aviv <avivhauyn@gmail.com>
2024-06-27 16:19:59 +0300

    added foliage spreader tool

--- Assets/_Scripts/Editor/GridBuilderTool.cs
+++ Assets/_Scripts/Editor/GridBuilderTool.cs
@@ -12,12 +12,13 @@ public class GridBuilderTool : EditorWindow, IBrushTool
     GridObject activeGridObject;
     static GridBuilderTool instance;
 
-    string[] tools = { "World Sculpting", "Building Placer" };
+    string[] tools = { "World Sculpting", "Building Placer", "Foliage Spreader" };
     ActiveTool activeTool;
     enum ActiveTool
     {
         WorldSculpting,
         BuildingPlacer,
+        FoliageSpreader
     }
 
 
@@ -42,6 +43,13 @@ public class GridBuilderTool : EditorWindow, IBrushTool
     Cell cornerCell;
     Cell lastCell;
 
+    //Foliage Spreader
+
+    List<GameObject> foliagePrefabs = new List<GameObject>();
+    List<Texture2D> foliageIcons = new List<Texture2D>();
+    int selectedFoliage = 0;
+    float coveragePercentage = 20;
+
 
     [MenuItem("Tools/Grid/Grid Builder Tool")]
     public static void ShowWindow()
@@ -65,20 +73,202 @@ public class GridBuilderTool : EditorWindow, IBrushTool
             DrawWorldSculptingToolGUI();
         }
 
-        if (activeTool == ActiveTool.BuildingPlacer)
+        else if (activeTool == ActiveTool.BuildingPlacer)
         {
             DrawBuildingPlacerToolGUI();
         }
+
+        else if (activeTool == ActiveTool.FoliageSpreader)
+        {
+            DrawFoliageSpreaderToolGUI();
+        }
+
     }
+
+    #region FoliageSpreader
+
+    void DrawFoliageSpreaderToolGUI()
+    {
+        if (GUILayout.Button("Recalculate Cell Usage"))
+        {
+            gridManager.RecalculateCellUsage();
+        }
+
+        DrawFoliageSpreaderDragAndDropArea();
+
+        if (foliagePrefabs.Count > 0)
+        {
+            if (GUILayout.Button("Start Placing"))
+            {
+                BrushToolManager.DisableAllBrushTools();
+                isPainting = !isPainting;
+            }
+            if (GUILayout.Button("Remove Selected"))
+            {
+                foliageIcons.RemoveAt(selectedFoliage);
+                foliagePrefabs.RemoveAt(selectedFoliage);
+            }
+
+            brushSize = EditorGUILayout.FloatField("Radius", brushSize);
+            coveragePercentage = EditorGUILayout.Slider("Coverage", coveragePercentage, 1, 100);
+
+            // Begin the scroll view
+            buildingScrollPos = GUILayout.BeginScrollView(buildingScrollPos);
+
+            // Define the fixed size for each element in the selection grid
+            int numberOfColumns = 4;
+            float buttonWidth = 51.2f;
+            float buttonHeight = 51.2f;
+
+            // Create a grid with fixed size buttons
+            selectedFoliage = GUILayout.SelectionGrid(selectedFoliage, foliageIcons.ToArray(), numberOfColumns,
+                GUILayout.Width(buttonWidth * numberOfColumns),
+                GUILayout.Height(buttonHeight * Mathf.Ceil(foliageIcons.Count / (float)numberOfColumns)));
+
+            // End the scroll view
+            GUILayout.EndScrollView();
+        }
+    }
+
+    void DrawFoliageSpreaderDragAndDropArea()
+    {
+        Event evt = Event.current;
+        Rect dropArea = GUILayoutUtility.GetRect(0.0f, 50.0f, GUILayout.ExpandWidth(true));
+        GUI.Box(dropArea, "Drag and Drop GameObjects here");
+
+        switch (evt.type)
+        {
+            case EventType.DragUpdated:
+            case EventType.DragPerform:
+                if (!dropArea.Contains(evt.mousePosition))
+                    return;
+
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                if (evt.type == EventType.DragPerform)
+                {
+                    DragAndDrop.AcceptDrag();
+
+                    foreach (GameObject draggedObject in DragAndDrop.objectReferences)
+                    {
+                        if (!foliagePrefabs.Contains(draggedObject))
+                        {
+                            foliagePrefabs.Add(draggedObject);
+                            Texture2D icon = AssetPreview.GetAssetPreview(draggedObject);
+                            while (AssetPreview.IsLoadingAssetPreview(draggedObject.GetInstanceID()) == true || icon == null)
+                            {
+                                Debug.Log("Waiting for asset preview to load");
+                                icon = AssetPreview.GetAssetPreview(draggedObject);
+                            }
+                            foliageIcons.Add(icon);
+                        }
+                    }
+                }
+                Event.current.Use();
+                break;
+        }
+    }
+
+    void FoliageSpreaderSceneGUI()
+    {
+        if (isPainting)
+        {
+            Event e = Event.current;
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+            if (e.type == EventType.MouseDown && e.button == 1)
+            {
+                isPainting = false;
+            }
+
+            Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerManager.GroundLayerMask))
+            {
+                Vector3 hitPoint = hit.point;
+                Handles.color = Color.green;
+                Handles.DrawWireDisc(hitPoint, Vector3.up, brushSize);
+
+                if (e.type == EventType.MouseDown && e.button == 0)
+                {
+                    activeGridObject = gridManager.GetCellFromPosition(hitPoint).grid;
+                    List<Cell> cells = GetCellsInCircle(hitPoint);
+                    Place(cells);
+                    e.Use();
+                }
+            }
+        }
+        SceneView.RepaintAll();
+    }
+
+    private void Place(List<Cell> cells)
+    {
+        if (cells.Count == 0) return;
+        Undo.RegisterCompleteObjectUndo(cells[0].grid, $"Created {foliagePrefabs[selectedFoliage].name}");
+        foreach (Cell cell in cells)
+        {
+            GameObject placedObject = PrefabUtility.InstantiatePrefab(foliagePrefabs[selectedFoliage]) as GameObject;
+            placedObject.transform.position = cell.position;
+            placedObject.transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 180), 0));
+
+            ICellOccupier occupier = placedObject.GetComponent<ICellOccupier>();
+            occupier.GetOccupiedCells();
+            occupier.OnOccupy();
+
+            Undo.RegisterCreatedObjectUndo(placedObject, $"Created {foliagePrefabs[selectedFoliage].name}");
+        }
+
+    }
+
+    List<Cell> GetCellsInCircle(Vector3 center)
+    {
+        List<Cell> cells = new List<Cell>();
+        for (int x = 0; x < gridManager.worldSettings.gridXSize; x++)
+        {
+            for (int y = 0; y < gridManager.worldSettings.gridYSize; y++)
+            {
+                Cell cell = activeGridObject.GetCellFromIndex(x, y);
+                if (cell != null)
+                {
+                    float distance = Vector3.Distance(VectorUtility.FlattenVector(cell.position), VectorUtility.FlattenVector(center));
+                    if (distance <= brushSize)
+                    {
+                        if (!cells.Contains(cell) && cell.IsFreeAndExists())
+                            cells.Add(cell);
+                    }
+                }
+            }
+        }
+        return TakeCoverageIntoAccount(cells);
+    }
+
+    List<Cell> TakeCoverageIntoAccount(List<Cell> cells)
+    {
+        List<Cell> selectedCells = new List<Cell>();
+        int cellsToSelect = Mathf.CeilToInt(cells.Count * (coveragePercentage / 100));
+        for (int i = 0; i < cellsToSelect; i++)
+        {
+            int index = Random.Range(0, cells.Count);
+
+            selectedCells.Add(cells[index]);
+            cells.RemoveAt(index);
+        }
+
+        return selectedCells;
+    }
+
+    #endregion
 
     void OnSceneGUI(SceneView sceneView)
     {
         if (gridManager == null) return;
         if (activeTool == ActiveTool.WorldSculpting)
-            SculptWorld();
+            SculptWorldSceneGUI();
         else if (activeTool == ActiveTool.BuildingPlacer)
-            BuildingPlacer();
+            BuildingPlacerSceneGUI();
+        else if (activeTool == ActiveTool.FoliageSpreader)
+            FoliageSpreaderSceneGUI();
     }
+
+
 
     #region BuildingPlacer
 
@@ -89,7 +279,7 @@ public class GridBuilderTool : EditorWindow, IBrushTool
             gridManager.RecalculateCellUsage();
         }
 
-        DrawDragAndDropArea();
+        DrawBuildingPlacerDragAndDropArea();
 
         if (buildingDatas.Count > 0)
         {
@@ -118,7 +308,7 @@ public class GridBuilderTool : EditorWindow, IBrushTool
 
     }
 
-    void DrawDragAndDropArea()
+    void DrawBuildingPlacerDragAndDropArea()
     {
         Event evt = Event.current;
         Rect dropArea = GUILayoutUtility.GetRect(0.0f, 50.0f, GUILayout.ExpandWidth(true));
@@ -155,7 +345,7 @@ public class GridBuilderTool : EditorWindow, IBrushTool
         }
     }
 
-    void BuildingPlacer()
+    void BuildingPlacerSceneGUI()
     {
         if (isPlacing)
         {
@@ -254,9 +444,11 @@ public class GridBuilderTool : EditorWindow, IBrushTool
     {
         activeGridObject.TryGetCells((Vector2Int)cell, buildingData.xSize, buildingData.ySize, out List<Cell> cells);
 
+        Undo.RegisterCompleteObjectUndo(cell.grid, $"Created {buildingDatas[selectedBuilding].name}");
+
         BuildingObject placed = BuildingObject.MakeInstance(buildingData, cell.position, cells);
         Undo.RegisterCreatedObjectUndo(placed.gameObject, $"Created {buildingDatas[selectedBuilding].name}");
-        Undo.RegisterCompleteObjectUndo(cell.grid, $"Created {buildingDatas[selectedBuilding].name}");
+
     }
 
     #endregion
@@ -297,7 +489,7 @@ public class GridBuilderTool : EditorWindow, IBrushTool
         isPlacing = false;
     }
 
-    void SculptWorld()
+    void SculptWorldSceneGUI()
     {
         if (isPainting)
         {
@@ -370,6 +562,8 @@ public class GridBuilderTool : EditorWindow, IBrushTool
 
     void ApplyVisibilityChanges()
     {
+        Undo.RegisterCompleteObjectUndo(activeGridObject, "World Sculpt");
+        
         bool visibility = isRaising;
 
         activeGridObject.ChangeCellsVisibility(selectedCells, visibility);
