eeafec5bc3e6e71e53de695aa2fa884ca1efd9b1
Aviv <avivhauyn@gmail.com>
2024-07-14 13:37:56 +0300

    Updated scriptable objects

--- Assets/_Scripts/BuildingSystem/Buildings/FloorTile.cs
+++ Assets/_Scripts/BuildingSystem/Buildings/FloorTile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Building/FloorTile")]
+[CreateAssetMenu(menuName = "Placeable/FloorTile")]
 public class FloorTile : PlaceableData
 {
     
--- Assets/_Scripts/BuildingSystem/Buildings/Placeable.cs
+++ Assets/_Scripts/BuildingSystem/Buildings/Placeable.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PlaceableData : ScriptableObject
 {
-    public string placeableName;
-    public Texture icon;
-    public int xSize = 1;
-    public int ySize = 1;
-    public bool takesFullCell;
-    public bool walkable;
-    public List<ItemCost> costs = new List<ItemCost>();
+    [ShowAssetPreview, BoxGroup("Main Info")] public Texture icon;
+    [BoxGroup("Main Info")] public string placeableName;
+    [ResizableTextArea, BoxGroup("Main Info")] public string description;
+    [BoxGroup("Main Info")] public List<ItemCost> costs = new List<ItemCost>();
+
+    [BoxGroup("Settings")] public int xSize = 1;
+    [BoxGroup("Settings")] public int ySize = 1;
+    [BoxGroup("Settings")] public bool takesFullCell;
+    [BoxGroup("Settings")] public bool walkable;
+    [BoxGroup("Settings")] public PlacementType placementType = PlacementType.Single;
+}
+
+public enum PlacementType
+{
+    Single,
+    Line,
+    Square
 }
