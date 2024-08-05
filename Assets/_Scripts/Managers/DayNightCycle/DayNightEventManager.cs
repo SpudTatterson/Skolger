58aec3cb02a45db04a1e9abefbeb0dda330fc3f6
Aviv <avivhauyn@gmail.com>
2024-08-05 20:50:31 +0300

    Added logic for getting events by name or time

--- Assets/_Scripts/Managers/DayNightCycle/DayNightEventManager.cs
+++ Assets/_Scripts/Managers/DayNightCycle/DayNightEventManager.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sydewa;
+using UnityEngine.Events;
 
-public class DayNightEventManager : MonoBehaviour
+public class DayNightEventManager : MonoSingleton<DayNightEventManager>
 {
     [SerializeField] float eventsTolerance = 0.2f;
     [SerializeField, Range(0f, 24f)] float ResetEventsTime = 0.1f;
     [SerializeField] List<EventInfo> events;
     bool DayCycleCompleted;
+    Dictionary<float, EventInfo> eventsSortedByTime = new Dictionary<float, EventInfo>();
+    Dictionary<string, EventInfo> eventsSortedByNames = new Dictionary<string, EventInfo>();
 
     public void Initialize(List<EventInfo> eventList)
     {
@@ -48,6 +51,35 @@ public class DayNightEventManager : MonoBehaviour
         foreach (var eventInfo in events)
         {
             eventInfo.executed = false;
+        }
+    }
+
+    public UnityEvent GetEvent(string eventName)
+    {
+        if (eventsSortedByNames.TryGetValue(eventName, out var eventInfo))  
+            return eventInfo.Event;
+        throw new System.Exception("Failed to find event as none match that name");
+    }
+    public UnityEvent GetEvent(float timeOfDay)
+    {
+        if (eventsSortedByTime.TryGetValue(timeOfDay, out var eventInfo))
+            return eventInfo.Event;
+        throw new System.Exception("Failed to find event as none match the specified time");
+    }
+
+    void OnValidate()
+    {
+        eventsSortedByNames.Clear();
+        eventsSortedByTime.Clear();
+
+        foreach (var eventInfo in events)
+        {
+            if (eventsSortedByNames.ContainsKey(eventInfo.eventName) || eventsSortedByTime.ContainsKey(eventInfo.Time))
+            {
+                throw new System.Exception("More then one event with the same name or time exist please remove one");
+            }
+            eventsSortedByNames.Add(eventInfo.eventName, eventInfo);
+            eventsSortedByTime.Add(eventInfo.Time, eventInfo);
         }
     }
 }
