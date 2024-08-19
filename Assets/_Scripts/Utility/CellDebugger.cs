737f0d37e18a4131b270b8a0db6d791ee7e631c1
Aviv <avivhauyn@gmail.com>
2024-08-19 11:31:26 +0300

    Added Cell debugger

---
+++ Assets/_Scripts/Utility/CellDebugger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellDebugger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    Cell cell = GridManager.Instance.GetCellFromPosition(hit.point);
+                    Debug.Log($"{cell}, {cell.cellType} in use {cell.inUse} has floor {cell.hasFloor}");
+                }
+            }
+        }
+    }
+}
