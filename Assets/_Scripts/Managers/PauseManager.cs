d13a8d31bc0b0b1ee735a5a3eaf8e0fabdedc4c3
Aviv <avivhauyn@gmail.com>
2024-10-10 20:07:16 +0300

    Visual time buttons should now update when pausing and unpausing

--- Assets/_Scripts/Managers/PauseManager.cs
+++ Assets/_Scripts/Managers/PauseManager.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Skolger.UI.Tabs;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Rendering;
 
 public class PauseManager : MonoSingleton<PauseManager>
 {
+    [SerializeField] TabGroup timeControlTabGroup;
+    [SerializeField] Skolger.UI.Tabs.TabButton regularSpeedTab;
     public UnityEvent OnPause;
     public UnityEvent OnResume;
     public UnityEvent OnPauseMenu;
@@ -38,6 +36,7 @@ public class PauseManager : MonoSingleton<PauseManager>
         if (paused)
         {
             UnPause();
+            timeControlTabGroup?.OnTabSelected(regularSpeedTab);
         }
         else
         {
